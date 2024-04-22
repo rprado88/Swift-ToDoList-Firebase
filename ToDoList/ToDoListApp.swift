fb8b40b2c9dc80ca782413e3a404ea3ae03bd875
Rodrigo Prado <32552146+rprado88@users.noreply.github.com>
2024-04-22 08:25:39 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Rodrigo Prado on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
